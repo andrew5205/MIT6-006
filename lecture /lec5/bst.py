bca49db8777184e8ec6712b9c8e0f0792a3015d1
Andrew5205 <andrewchung11@gmail.com>
2021-04-24 10:47:31 -0700

    add bst.py BSTnode + insert()

---
+++ lecture /lec5/bst.py
@@ -0,0 +1,53 @@
+class BST(object):
+    """
+Simple binary search tree implementation.
+This BST supports insert, find, and delete-min operations.
+Each tree contains some (possibly 0) BSTnode objects, representing nodes,
+and a pointer to the root.
+"""
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, t):
+        """Insert key t into this BST, modifying it in-place."""
+        new = BSTnode(t)
+        if self.root is None:
+            self.root = new
+        else:
+            node = self.root
+            while True:
+                if t < node.key:
+                    # Go left
+                    if node.left is None:
+                        node.left = new
+                        new.parent = node
+                        break
+                    node = node.left
+                else:
+                    # Go right
+                    if node.right is None:
+                        node.right = new
+                        new.parent = node
+                        break
+                    node = node.right
+        return new
+    
+    
+    
+    
+    
+    
+class BSTnode(object):
+    """
+Representation of a node in a binary search tree.
+Has a left child, right child, and key value.
+"""
+    def __init__(self, t):
+        """Create a new leaf with key t."""
+        self.key = t
+        self.disconnect()
+    def disconnect(self):
+        self.left = None
+        self.right = None
+        self.parent = None
