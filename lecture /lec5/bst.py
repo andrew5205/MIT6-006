6fd7ea0ddc718d6fca24b9ee2e28e1fe31532bad
Andrew5205 <andrewchung11@gmail.com>
2021-05-22 11:00:45 -0700

    update bst.py

--- lecture /lec5/bst.py
+++ lecture /lec5/bst.py
@@ -32,7 +32,7 @@ and a pointer to the root.
                         break
                     node = node.right
         return new
-    
+
     def find(self, t):
         """Return the node for key t if is in the tree, or None otherwise."""
         node = self.root
@@ -44,10 +44,66 @@ and a pointer to the root.
             else:
                 node = node.right
         return None
-    
-    
-    
-    
+
+    def delete_min(self):
+        """Delete the minimum key (and return the old node containing it)."""
+        if self.root is None:
+            return None, None
+        else:
+            # Walk to leftmost node.
+            node = self.root
+            while node.left is not None:
+                node = node.left
+            # Remove that node and promote its right subtree.
+            if node.parent is not None:
+                node.parent.left = node.right
+            else: # The root was smallest.
+                self.root = node.right
+            if node.right is not None:
+                node.right.parent = node.parent
+            parent = node.parent
+            node.disconnect()
+            return node, parent
+
+    def __str__(self):
+        if self.root is None: 
+            return '<empty tree>'
+        
+        def recurse(node):
+            if node is None: 
+                return [], 0, 0
+            
+            label = str(node.key)
+            left_lines, left_pos, left_width = recurse(node.left)
+            right_lines, right_pos, right_width = recurse(node.right)
+            middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
+            pos = left_pos + middle // 2
+            width = left_pos + middle + right_width - right_pos
+            
+            while len(left_lines) < len(right_lines):
+                left_lines.append(' ' * left_width)
+            while len(right_lines) < len(left_lines):
+                right_lines.append(' ' * right_width)
+            if (middle - len(label)) % 2 == 1 and node.parent is not None and node is node.parent.left and len(label) < middle:
+                label += '.'
+                
+            label = label.center(middle, '.')
+            
+            if label[0] == '.': 
+                label = ' ' + label[1:]
+            if label[-1] == '.': 
+                label = label[:-1] + ' '
+            
+            lines = [' ' * left_pos + label + ' ' * (right_width - right_pos),
+                     ' ' * left_pos + '/' + ' ' * (middle-2) +
+                     '\\' + ' ' * (right_width - right_pos)] + \
+              [left_line + ' ' * (width - left_width - right_width) + right_line
+            
+            for left_line, right_line in zip(left_lines, right_lines)]
+            return lines, pos, width
+        
+        return '\n'.join(recurse(self.root) [0])
+
 class BSTnode(object):
     """
 Representation of a node in a binary search tree.
